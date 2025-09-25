00dbbe42ff4645c627b59ab5437514857c1893c6
Carlomos7 <cs525@njit.edu>
2025-09-25 13:17:20 -0400

    Refac: seperated passwordencoder bean

--- src/main/java/com/example/banking/config/DevSecurityConfig.java
+++ src/main/java/com/example/banking/config/DevSecurityConfig.java
@@ -6,21 +6,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @Profile("dev")
 public class DevSecurityConfig {
-
-    
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
